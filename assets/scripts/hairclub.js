bba04f4872c02c47c14fbb95d8dab169ce9c6fd6
Jorge <jorge@mexbowl.com>
2022-08-01 22:28:36 -0500

    hc updates

--- assets/scripts/hairclub.js
+++ assets/scripts/hairclub.js
@@ -1,9 +1,3 @@
-
-// ALAN variables
-
-var public_id = "{{ page.end_user_public_id }}";
-var internal_id = "{{ page.end_user_internal_id }}";
-
 $(".lp").attr("data-internal-id", internal_id);      
 $(".lp").attr("data-public-id",public_id);
 
@@ -206,3 +200,13 @@ $("#form").submit(function(e) {
   });
 
 }); // End form submission
+
+// Comparison
+
+$("#slider").on("input change", (e) => {
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $(".foreground-img").css("width", `${sliderPos}%`);
+  // Update the position of the slider button
+  $(".slider-button").css("left", `calc(${sliderPos}% - 18px)`);
+});
