0e9aed90aad41e765d9f525c7d6379fbb068abdd
Jorge <jorge@mexbowl.com>
2022-08-01 23:40:20 -0500

    hc updates

--- assets/scripts/hairclub.js
+++ assets/scripts/hairclub.js
@@ -208,5 +208,14 @@ $("#slider").on("input change", (e) => {
   // Update the width of the foreground image
   $(".foreground-img").css("width", `${sliderPos}%`);
   // Update the position of the slider button
-  $(".slider-button").css("left", `calc(${sliderPos}% - 18px)`);
+  $(".slider-button").css("left", `${sliderPos}%`);
+});
+
+// Scroll to Top
+
+$(".footer-cta-button").click(function() {
+  $("html, body").animate({
+    scrollTop: 0
+  }, 500);
+  return false;
 });
